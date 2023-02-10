0e78d727b64f28056b4a7b77e9a04642e175b17e
suhwan2004 <suhwan2004@gmail.com>
2023-02-10 09:45:20 +0900

    불필요한 주석 제거

--- ALGO_DS/ALGO/Math/bitmast-subset.js
+++ ALGO_DS/ALGO/Math/bitmast-subset.js
@@ -1,24 +1,3 @@
-// import java.io.*;
-// public class 김수환 {
-
-// 	public static void main(String[] args) throws IOException {
-// 		// TODO Auto-generated method stub
-// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-// 		StringBuilder sb = new StringBuilder();
-// 		int len = Integer.parseInt(br.readLine());
-// 		String[] input = br.readLine().split(" ");
-
-// 		for (int i = 0; i < (1 << len); i++) {
-// 			for (int j = 0; j < len; j++) {
-// 				sb.append((i & (1 << j)) != 0 ? input[j] : "X").append("\t");
-// 			}
-// 			sb.append("\n");
-// 		}
-
-// 		System.out.println(sb);
-// 	}
-// }
-
 function bitmastSubset(len) {
   const input = new Array(len).fill(0);
   const res = [];
