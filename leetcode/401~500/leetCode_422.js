ad98a950e8e5c40842fa952ab3c603b5986da0dd
suhwan2004 <suhwan2004@gmail.com>
2023-03-07 22:01:22 +0900

    2023-03-07 leetCode_422.js 추가로직 갱신

--- leetcode/401~500/leetCode_422.js
+++ leetcode/401~500/leetCode_422.js
@@ -8,3 +8,28 @@ var validWordSquare = function (words) {
   }
   return true;
 };
+
+//아래는 2023.03.07일자 풀이
+
+/*
+start : 2023-03-07 21 : 43 ~ 21 : 58
+*/
+var validWordSquare = function (words) {
+  let rowLen = words.length;
+  let colLen = rowLen;
+
+  words.forEach((word) => {
+    colLen = Math.max(word.length, colLen);
+  });
+
+  if (rowLen != colLen) return false;
+  if (rowLen === 1 && colLen === 1) return true;
+
+  for (let i = 0; i < rowLen - 1; i++) {
+    for (let j = i + 1; j < rowLen; j++) {
+      if (words[i][j] != words[j][i]) return false;
+    }
+  }
+
+  return true;
+};
